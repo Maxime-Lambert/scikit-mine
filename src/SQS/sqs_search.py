37ae67ce8bad43e19cc2c1593372e7ff8ebf1d85
mpiscot <Gamejeux14>
2019-01-30 17:24:14 +0100

    ajout de sqs_search

---
+++ src/SQS/sqs_search.py
@@ -0,0 +1,44 @@
+def sqs_search(data):
+    p_array = []
+    a = []
+    changes = True
+    gap_event = []
+
+    for sequence in data:
+        ct = build_window(sequence)
+
+    while changes :
+        f = []
+        for pattern in ct:
+            f.append(estimate(pattern, a, data))
+        f.sort()
+
+        for pattern in f:
+            if l(data, pattern + p_array + ct) < l(data, p_array + ct):
+                p_array = prune(p_array + pattern , data, False)
+                if(p_array.contains(pattern)):
+                    scan_for_gaps(pattern + gap_event)
+
+
+def build_window(sequence):
+    res = []
+    for item in sequence:
+        if item not in res:
+            res.append(item)
+    return res
+
+
+def estimate(pattern, a, data):
+    pass
+
+
+def l(data, pattenr):
+    pass
+
+
+def prune(pattern, data, bool):
+    pass
+
+
+def scan_for_gaps(pattern):
+    pass

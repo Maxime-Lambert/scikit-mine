61720fd97d5d335f03ed351ffa9d83bb0a2406e6
mpiscot <Gamejeux14>
2019-01-30 17:07:57 +0100

    début du squelette de sqs

---
+++ src/SQS/sqs.py
@@ -0,0 +1,38 @@
+def sqs(data, patterns):
+    changes = True
+    for sequence in data:
+        sequence.set_usage(find_usage(sequence, data))
+    for pattern in patterns:
+        if(pattern.length > 0):
+            list_window = find_windows(pattern, data)
+            pattern.set_usage(list_window.length)
+            pattern.set_gap(pattern.length - 1)
+    w = merge(list_window)
+
+    while changes:
+        a = align(w)
+
+    return a
+
+
+def find_usage(sequence, data):
+    res = 0
+    for s in data:
+        if s == sequence:
+            res += 1
+    return res
+
+
+def find_windows(pattern, data):
+    pass
+
+
+def align(window):
+    pass
+
+
+def merge(window):
+    res = []
+    for item in window:
+        res += [item]
+    return res

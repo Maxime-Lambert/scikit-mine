d6d59355055347e76957c163ef45d596562a95c5
Giraud Pieric <pieric.giraud@etudiant.univ-rennes1.fr>
2019-02-13 15:29:01 +0100

    Update test.py

---
+++ src/SQS/test.py
@@ -0,0 +1,20 @@
+import unittest
+
+from .fichier import *
+from .database import *
+
+class MyTest(unittest.TestCase):
+    
+    def test_sequence_windows(self):
+        test = [1]
+        self.assertEquals(test, build_window(test))
+        self.assertEquals([1], build_window(test))
+        
+    def test_sequence_search(self):
+        test = [[1]]
+        self.assertEquals(test, sqs_search(test))
+        self.assertEquals([1], sqs_search())
+        
+        
+if __name__ == '__main__':
+    unittest.main()

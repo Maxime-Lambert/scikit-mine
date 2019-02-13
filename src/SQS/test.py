22a848a5db894f9c35dde37ea897c507e8527cb8
Giraud Pieric <pieric.giraud@etudiant.univ-rennes1.fr>
2019-02-13 15:46:34 +0100

    Update test.py

--- src/SQS/test.py
+++ src/SQS/test.py
@@ -1,21 +1,26 @@
 import unittest
 
-from .fichier import *
-from .database import *
 from sqs_search import *
 
 class MyTest(unittest.TestCase):
     
     def test_sequence_windows(self):
         test = [1]
+        mongo = "mongo"
         self.assertEquals(test, build_window(test))
         self.assertEquals([1], build_window(test))
+        with self.assertRaises(TypeError):
+            result = build_window(mongo)
+
         
     def test_sequence_search(self):
         test = [[1]]
+        bongo = "bongo"
         self.assertEquals(test, sqs_search(test))
         self.assertEquals([1], sqs_search())
-        
+        with self.assertRaises(TypeError):
+            result = sqs_search(bongo)
+
         
 if __name__ == '__main__':
     unittest.main()

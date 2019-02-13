2bcd3d69629ed89aed1f9052baa7dabbbe8a77ea
MagikarpLvl5 <36775948+MagikarpLvl5@users.noreply.github.com>
2019-02-13 14:39:18 +0100

    Update CodeTable.py

--- src/CodeTable.py
+++ src/CodeTable.py
@@ -3,6 +3,8 @@
 from Pattern import *
 from Database import *
 
+import math
+
 
 class CodeTable:
 
@@ -15,43 +17,70 @@ class CodeTable:
             res = res + k.repr + " " + v.toString() + " "
         return res
 
-    def contains(self, pattern):
-        return pattern in self.patternMap
-
-    def size(self):
-        return len(self.patternMap)
-
     def add(self, pattern):
-        if(pattern in self.patternMap):
+        b = False
+        for k, v in patternMap:
+            if pattern.eq(k):
+                b = True
+        if b:
             self.patternMap[pattern].usage += 1
         else:
             self.patternMap[pattern] = 0
         return self
 
     def remove(self, pattern):
-        if(pattern in self.patternMap):
+        if pattern in self.patternMap:
             del self.patternMap[pattern]
         return self
+
+    def contains(self, pattern):
+        return pattern in self.patternMap
+
+    def size(self):
+        return len(self.patternMap)
 
     def get(self, number):
         i = 0
         for k, v in self.patternMap.items():
-            if(i == number):
+            if i == number:
                 return k
             else:
                 i += 1
         return -1
 
+    def usageSum(self):
+        i = 0
+        for k, v in self.patternMap.items():
+            i += k.usage
+        return i
+
     def calculateCodeLength(self):
+        for k, v in self.patternMap.items():
+            self.patternMap[k] = bytes(-math.log2(k.usage/self.usageSum()))
         return self
 
-    def codeTableLength(self, codeTable):
-        return self
+    def databaseEncodedLength(self):
+        self.calculateCodeLength()
+        i = 0
+        for k, v in self.patternMap.items():
+            i += (k.usage * v)
+        return i
 
-    def compareGain(self, codeTable, data, compression1, compression2):
-        compression1
-        compression2
-        if(compression1 > compression2):
+    def codeTableLength(self, sct):
+        self.calculateCodeLength()
+        sct.calculateCodeLength()
+        i = 0
+        for k, v in self.patternMap.items():
+            for p in k.elements:
+                for x, y in sct.patternMap.items():
+                    if(p == x):
+                        i += y
+        return i
+
+    def compareGain(self, codeTable, data):
+        compression1 = 0
+        compression2 = 0
+        if compression1 > compression2:
             return True
         return False
 
@@ -59,9 +88,21 @@ class CodeTable:
         for k, v in self.patternMap:
             ct = self
             del ct.patternMap[k]
-            if(self.compareGain(ct, data)):
+            if self.compareGain(ct, data):
                 self.patternMap = ct
                 return self
 
     def orderByUsage(self):
         return sorted(self.patternMap, key=self.patternMap.usage)
+    
+    def repeats(string):
+    # Lower the case in the string
+    string = string.lower()
+ 
+    # Get a set of the unique letters
+    uniques = set(string)
+ 
+    # Count the max occurrences of each unique letter
+    counts = [string.count(letter) for letter in uniques]
+ 
+    return max(counts)

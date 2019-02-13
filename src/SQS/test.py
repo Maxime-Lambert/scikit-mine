dc95169e501f7d7f1344096684966df8c043f555
Giraud Pieric <pieric.giraud@etudiant.univ-rennes1.fr>
2019-02-13 15:54:24 +0100

    Update test.py

--- src/SQS/test.py
+++ src/SQS/test.py
@@ -6,20 +6,29 @@ class MyTest(unittest.TestCase):
     
     def test_sequence_windows(self):
         test = [1]
-        mongo = "mongo"
+        bingo = "bingo"
         self.assertEquals(test, build_window(test))
         self.assertEquals([1], build_window(test))
+        self.assertEquals([1], build_windows([1, 1, 1]))
         with self.assertRaises(TypeError):
-            result = build_window(mongo)
+            result = build_window(bingo)
 
         
     def test_sequence_search(self):
         test = [[1]]
-        bongo = "bongo"
+        bango = "bango"
         self.assertEquals(test, sqs_search(test))
         self.assertEquals([1], sqs_search())
         with self.assertRaises(TypeError):
-            result = sqs_search(bongo)
+            result = sqs_search(bango)
+            
+    def test_sequence_estimae(self):
+        test = [1]
+        bongo = "bongo"
+            
+    def test_sequence_prune(self):
+        test = [1]
+        congo = "congo"
 
         
 if __name__ == '__main__':

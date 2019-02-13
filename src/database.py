a999e118dd7f14348c7a520cfc3686e3c6f40162
Darlysia <darlysia@users.noreply.github.com>
2019-02-13 15:10:23 +0100

    comment database

--- src/database.py
+++ src/database.py
@@ -6,34 +6,53 @@ Created on Mon Feb 11 14:45:31 2019
 
 @author: Josie Signe
 """
-from src.CodeTable import *
-from src.patern import *
+from CodeTable import CodeTable
+from patern import Pattern
 
 
 class Database:
+    """Database class
+
+    Use derived classes?
+
+    Parameters
+    ----------
+    trans_collection : object, optional (default=None)
+        The base estimator from which the ensemble is built.
+
+    Attributes
+    ----------
+    trans_collection_ : estimator
+        The base estimator from which the ensemble is grown.
+    """
 
     def __init__(self, int_trans_list):
-        self.trans_collection = int_trans_list
+        self.trans_collection_ = int_trans_list
 
-    def standard_code_table(self):
+    def _make_standard_code_table(self):
+        """Make and return the standard code table of the database."""
         sct = CodeTable()  # map pattern code
         # On ajoute les singletons de la base à la SCT
-        for trans in self.trans_collection:
+        for trans in self.trans_collection_:
             # les trans sont des ensemble d'int
             for item in trans:
                 pattern = Pattern([item])
-                if not sct.contains(pattern):
-                    sct.add(pattern)
+                sct.add(pattern)
                 pattern.usage = pattern.usage + 1  # ne fonctionne pas
-                print(pattern.usage)
             # puis calcul des codes de la sct
         return sct
 
     def __repr__(self):
-        return repr(self.trans_collection)
+        return repr(self.trans_collection_)
 
     def __len__(self):
+        """Returns the number of transaction of the database."""
         return len(self.trans_collection)
 
-    def __getitem__(self, transaction):
-        return self.trans_collection[transaction]
+    def __getitem__(self, index_trans):
+        """Returns the transaction at index_trans"""
+        return self.trans_collection[index_trans]
+
+    def __iter__(self):
+        """Returns iterator over estimators in the ensemble."""
+        return iter(self.trans_collection)

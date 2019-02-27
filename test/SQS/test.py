485bb7d8476589a2bf769b5a281522bf8c4a7a7e
Giraud Pieric <pieric.giraud@etudiant.univ-rennes1.fr>
2019-02-27 11:43:53 +0100

    Update test/SQS/test.py

--- test/SQS/test.py
+++ test/SQS/test.py
@@ -1,6 +1,9 @@
 import unittest
 
 from src.SQS.sqs_search import *
+from src.Files import *
+from src.database import *
+
 
 class MyTest(unittest.TestCase):
     
@@ -30,6 +33,13 @@ class MyTest(unittest.TestCase):
         test = [1]
         congo = "congo"
 
+    def yvanresults(self):
+        enter = Fichier("testenter")
+        database1 = Database(enter.list_int)
+        result = sqs_search(database)
+        exit = Fichier("testexit")
+        database2 = Database(monfichier.list_int)
+        self.assertEquals(result, database2)
         
 if __name__ == '__main__':
     unittest.main()

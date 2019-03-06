7fe9e1ca7d27031b94f091eb59dde65ce1f2825b
Le Duff Yvan <yvan.le-duff@etudiant.univ-rennes1.fr>
2019-03-06 16:49:27 +0100

    Update diffnorm.sh test iteration0

--- scripts/diffnorm.sh
+++ scripts/diffnorm.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 python3 src/DiffNorm/Main.py
-pytest --junitxml=pytest/diffnorm.xml test/Diffnorm/test_DiffNorm.py
+pytest --junitxml=pytest/diffnorm.xml test/Diffnorm/test_iteration0.py
